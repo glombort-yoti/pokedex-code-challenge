249056b9268582144eb1d5cd4f7bf6b83f76c279
glombort-yoti <george.lambert@yoti.com>
2022-09-09 15:19:20 +0100

    pokedex

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const server = "https://raw.githubusercontent.com/ramclen/Poke-Server/master";
 
 function App() {
+  const [pokedex, setPokedex] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+
+  const search = (e) => {
+    const { value } = e.target;
+    setFiltered(
+      pokedex.filter((pokedex) => {
+        return pokedex.name.toLowerCase().includes(value.toLowerCase());
+      })
+    );
+  };
+
+  const getMoreInfo = (id) => {
+    setSelectedPokemon(pokedex[id - 1]);
+  };
+
+  const loadData = async () => {
+    let res = await fetch(`${server}/descriptions.json`).then((res) =>
+      res.json()
+    );
+
+    let resolve = await fetch(`${server}/pokedex.json`).then((res) =>
+      res.json()
+    );
+    resolve = resolve[3].Pokedex;
+
+    resolve = resolve.map((pokemon, index) => {
+      debugger;
+      pokemon.description = res[index].description;
+      pokemon.image = res[index].image;
+      return pokemon;
+    });
+
+    setPokedex(resolve);
+    setFiltered(resolve);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {selectedPokemon === null ? (
+        <>
+          <input type="text" onChange={search} />
+          <ul>
+            {filtered.map((pokemon) => (
+              <li
+                className="pokemon-list-item"
+                key={pokemon.id}
+                onClick={() => getMoreInfo(pokemon.id)}
+              >
+                {pokemon.name}
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <div>
+          <h1>{selectedPokemon.name}</h1>
+          <p>{selectedPokemon.description}</p>
+          <img
+            src={`${server}/${selectedPokemon.image.image}`}
+            alt={selectedPokemon.name}
+          />
+          <button type="button" onClick={() => setSelectedPokemon(null)}>
+            Back
+          </button>
+        </div>
+      )}
     </div>
   );
 }
